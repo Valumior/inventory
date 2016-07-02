6aedce01f6c871d978d1730160d02205e5347e7f
Valumior <matinoshadow@gmail.com>
2016-07-02 18:17:52 +0200

    QR gen fix

--- manager/views.py
+++ manager/views.py
@@ -210,7 +210,7 @@ def generateQrImage(request, pk=None):
 		
 		signing = entry.signing
 		qr = qrcode.QRCode(version=None, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-		qr.add_data(id_number)
+		qr.add_data(signing)
 		qr.make(fit=True)
 		
 		img = qr.make_image()
