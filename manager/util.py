39ffff249b95acdca760e98150f84ae2b24c4f8b
Valumior <matinoshadow@gmail.com>
2016-07-20 17:55:43 +0200

    Change in signing translation

--- manager/util.py
+++ manager/util.py
@@ -1,8 +1,8 @@
 import string
 
 def URLify_entry_signing(signing):
-	return string.replace(string.replace(signing,'/','-'),' ','_')
+	return string.replace(string.replace(signing,'/','__'),' ','_')
 	
 def deURLify_entry_signing(signing):
-	return string.replace(string.replace(signing,'-','/'),'_',' ')
+	return string.replace(string.replace(signing,'__','/'),'_',' ')
 	
