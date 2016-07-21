f901e882950fe9dbd83186de7e4c40ab9891832c
Valumior <matinoshadow@gmail.com>
2016-07-21 17:09:37 +0200

    Table styling fix

--- manager/tables.py
+++ manager/tables.py
@@ -4,75 +4,80 @@ from manager.models import *
 from manager.util import URLify_entry_signing
 
 button_attrs = { 'a' :  { 'class' : 'btn btn-default btn-block' }}
+table_attrs = { 'class' : 'table table-bordered table-condensed' }
 
-class BaseTable(tables.Table):
-	class Meta:
-		attrs = { 'class' : 'table table-bordered table-condensed' }
-
-class AddressTable(BaseTable):
+class AddressTable(tables.Table):
 	details = tables.LinkColumn('addressDetails', kwargs={ 'pk' : A('pk') }, orderable=False, text='Szczegoly', empty_values=(), attrs=button_attrs)
 	edit = tables.LinkColumn('editAddress', kwargs={ 'pk' : A('pk') }, orderable=False, text='Edytuj', empty_values=(), attrs=button_attrs)
 	
 	class Meta:
 		model = Address
 		fields = ('city', 'street', 'street_number', 'details', 'edit')
+		attrs = table_attrs
 		
 class AddressTableNoEdit(AddressTable):
 	class Meta:
 		exclude = ('edit',)
 
-class EntryTable(BaseTable):
+class EntryTable(tables.Table):
 	signing = tables.LinkColumn('entryDetails', kwargs={ 'pk' : A('getURL') }, attrs=button_attrs)
 	room = tables.LinkColumn('roomDetails', kwargs={ 'pk' : A('room.id') }, attrs=button_attrs)
 	
 	class Meta:
 		model = Entry
 		fields = ('signing', 'name', 'date_added', 'added_price', 'date_removed', 'removed_price', 'room', 'short_description', 'last_modified')
+		attrs = table_attrs
 
-class LogEntryTable(BaseTable):
+class LogEntryTable(tables.Table):
 	old_location = tables.LinkColumn('roomDetails', kwargs={ 'pk' : A('old_location.id') }, attrs=button_attrs)
 	new_location = tables.LinkColumn('roomDetails', kwargs={ 'pk' : A('new_location.id') }, attrs=button_attrs)
 	
 	class Meta:
 		model = LogEntry
 		fields = ('entry', 'log_date', 'old_location', 'new_location', 'notes', 'user')
+		attrs = table_attrs
 
-class RoomTable(BaseTable):
+class RoomTable(tables.Table):
 	room_id = tables.LinkColumn('roomDetails', kwargs={ 'pk' : A('pk') }, attrs=button_attrs)
 	edit = tables.LinkColumn('editRoom', kwargs={ 'pk' : A('pk') }, orderable=False, text='Edytuj', empty_values=(), attrs=button_attrs)
 	
 	class Meta:
 		model = Room
 		fields = ('room_id', 'address', 'edit')
+		attrs = table_attrs
 
 class RoomTableNoEdit(RoomTable):
 	class Meta:
 		exclude = ('edit',)
 
-class UserPermissionsTable(BaseTable):
+class UserPermissionsTable(tables.Table):
 	user = tables.LinkColumn('userDetails', kwargs={ 'pk' : A('user.id') }, attrs=button_attrs)
 	
 	class Meta:
 		model = UserPermissions
 		fields = ('user', 'is_admin', 'is_user_manager', 'is_add_allowed', 'is_edit_allowed', 'is_session_controller', 'is_inventory')
+		attrs = table_attrs
 
-class InventoryOrderTable(BaseTable):
+class InventoryOrderTable(tables.Table):
 	details = tables.LinkColumn('inventoryOrderReports', kwargs={ 'pk' : A('pk') }, orderable=False, text='Szczegoly', empty_values=(), attrs=button_attrs)
 	
 	class Meta:
 		model = InventoryOrder
 		fields = ('completed', 'date_ordered', 'date_completed', 'details')
+		attrs = table_attrs
 
-class InventoryRoomReportTable(BaseTable):
+class InventoryRoomReportTable(tables.Table):
 	details = tables.LinkColumn('inventoryReportDetails', kwargs={ 'pk' : A('pk') }, orderable=False, text='Szczegoly', empty_values=(), attrs=button_attrs)
 	
 	class Meta:
 		model = InventoryRoomReport
 		fields = ('room', 'date_posted', 'author', 'details')
+		attrs = table_attrs
 
-class InventoryEntryNoteTable(BaseTable):
+class InventoryEntryNoteTable(tables.Table):
 	entry = tables.LinkColumn('entryDetails', kwargs={ 'pk' : A('entry.getURL') }, attrs=button_attrs)
 	
 	class Meta:
 		model = InventoryEntryNote
 		fields = ('entry', 'status')
+		attrs = table_attrs
