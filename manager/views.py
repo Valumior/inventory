4eb67c63a79dff7d8713dfd40ec2b579af136553
Valumior <matinoshadow@gmail.com>
2016-07-24 16:18:36 +0200

    template and queryset fixes

--- manager/views.py
+++ manager/views.py
@@ -270,7 +270,10 @@ def entryDetailsView(request, pk=None):
 	if pk:
 		entry = get_object_or_404(Entry, signing=deURLify_entry_signing(pk))
 		logs = LogEntryTable(LogEntry.objects.filter(entry=entry))
-		liquidation = LiquidationEntryNote.objects.filter(entry=entry, liquidation__completed=False, liquidation__rejected=False).first().values('liquidation')
+		if LiquidationEntryNote.objects.filter(entry=entry, liquidation__completed=False, liquidation__rejected=False).exists():
+			liquidation = LiquidationEntryNote.objects.get(entry=entry, liquidation__completed=False, liquidation__rejected=False).liquidation
+		else:
+			liquidation = None
 		can_liquidate = Liquidation.objects.filter(submitted=False).exists()
 		RequestConfig(request).configure(logs)
 		return render(request, 'entryDetails.html', { 'entry' : entry , 'logs' : logs, 'image' : reverse('generateQr', kwargs={ 'pk' : pk }), 'liquidation' : liquidation , 'can_liquidate' : can_liquidate })
