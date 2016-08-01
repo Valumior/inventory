8f8d95b5cc4e0da62faa56e6ba4201a6cfa5082d
Valumior <matinoshadow@gmail.com>
2016-08-01 16:10:56 +0200

    settings register

--- manager/admin.py
+++ manager/admin.py
@@ -3,7 +3,6 @@ from manager.models import *
 
 # Register your models here.
 
-admin.site.register(UserPermissions)
 admin.site.register(Address)
 admin.site.register(Room)
 admin.site.register(Institution)
@@ -13,3 +12,5 @@ admin.site.register(InventoryOrder)
 admin.site.register(InventoryRoomReport)
 admin.site.register(InventoryEntryNote)
 admin.site.register(Liquidation)
+admin.site.register(UserPermissions)
+admin.site.register(UserSettings)
